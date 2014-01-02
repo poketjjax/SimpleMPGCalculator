488a91dcda71d0725584410db50f6df81b376002
Jackson Wright <jackson.wright69@gmail.com>
2014-01-01 20:07:50 -0600

    finished the view and got some basic functionality done

--- SimpleMPGCalculator/gen/com/jackson/simplempgcalculator/R.java
+++ SimpleMPGCalculator/gen/com/jackson/simplempgcalculator/R.java
@@ -24,11 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080007;
         public static final int fuelprice=0x7f080002;
         public static final int gallons=0x7f080001;
-        public static final int reset=0x7f080003;
-        public static final int saveResults=0x7f080004;
+        public static final int mpg=0x7f080003;
+        public static final int mpgtext=0x7f080004;
+        public static final int reset=0x7f080005;
+        public static final int saveResults=0x7f080006;
         public static final int tankmiles=0x7f080000;
     }
     public static final class layout {
@@ -46,6 +48,7 @@ public final class R {
         public static final int fuelprice=0x7f050008;
         public static final int gallons=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int mpg=0x7f05000b;
         public static final int reset=0x7f050009;
         public static final int save=0x7f05000a;
         public static final int tankmiles=0x7f050003;
--- SimpleMPGCalculator/src/com/jackson/simplempgcalculator/EnterMileage.java
+++ SimpleMPGCalculator/src/com/jackson/simplempgcalculator/EnterMileage.java
@@ -1,21 +1,97 @@
 package com.jackson.simplempgcalculator;
 
+import java.text.NumberFormat;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Service;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class EnterMileage extends Activity {
+public class EnterMileage extends Activity implements OnClickListener {
+	
+	//Decalre variables
+    public EditText miles;
+	public EditText gallons;
+	public EditText fuelprice;
+	public TextView mpg;
+	public TextView mpgtext;
+	public float milesint;
+	public float gallonsint;
+	public float fuelpriceint;
+	public float totalmpg;
+	public float mpgRounded;
+	private Button reset;
+	private Button submit;
+	public String mpgString;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_enter_mileage);
-		EditText miles = (EditText) findViewById(R.id.tankmiles);
+		
+		miles = (EditText) findViewById(R.id.tankmiles);
+		gallons = (EditText) findViewById(R.id.gallons);
+		fuelprice = (EditText) findViewById(R.id.fuelprice);
+		mpg = (TextView) findViewById(R.id.mpg);
+		mpgtext = (TextView) findViewById(R.id.mpgtext);
+		
+
+		//hide the mpg text until after it is calculated
+		mpgtext.setVisibility(View.GONE);
+		
+		//set the focus and pull up the keyboard for the first edit text field
 		miles.requestFocus();
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+		
+		//set onclicklistener for all buttons
+		reset = (Button) findViewById(R.id.reset);
+		submit = (Button) findViewById(R.id.saveResults);
+		
+		reset.setOnClickListener(this);
+		submit.setOnClickListener(this);
+	
+		//set onkeylistener so that enter on the last text field will calculate MPG
+		fuelprice.setOnKeyListener(new OnKeyListener()
+		{
+		    public boolean onKey(View v, int keyCode, KeyEvent event)
+		    {
+		        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+		                (keyCode == KeyEvent.KEYCODE_ENTER))
+		        {
+		        	milesint = Integer.parseInt(miles.getText().toString());
+		        	gallonsint = Integer.parseInt(gallons.getText().toString());
+		        	if(fuelprice.getText().toString().equalsIgnoreCase(""))
+		        	{
+		        		fuelpriceint = 0;
+		        	}
+		        	else
+		        	{
+		        		fuelpriceint = Integer.parseInt(fuelprice.getText().toString());
+		        	}
+		        	switch (keyCode)
+		            {
+		                case KeyEvent.KEYCODE_DPAD_CENTER:
+		                case KeyEvent.KEYCODE_ENTER:
+		                    calculateMPG(milesint, gallonsint, fuelpriceint);
+		                    return true;
+		                default:
+		                    break;
+		            }
+		        }
+		        return false;
+		    }
+		});
 		}
 
 	@Override
@@ -25,4 +101,48 @@ public class EnterMileage extends Activity {
 		return true;
 	}
 	
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.reset:
+			clearAllFields();
+			break;
+		case R.id.saveResults:
+			saveTheResults();
+			break;
+		}	
+	}
+	
+	public void clearAllFields() {
+		miles.setText("");
+		gallons.setText(""); 
+		fuelprice.setText("");	
+		
+		InputMethodManager imm = (InputMethodManager)this.getSystemService(Service.INPUT_METHOD_SERVICE);
+		
+		miles.requestFocus();
+		imm.showSoftInput(miles, 0);
+		mpgtext.setVisibility(View.GONE);
+		mpg.setText("");
+	}
+	
+	public void saveTheResults() {
+		
+	}
+	
+	public void calculateMPG(float milesint, float gallonsint, float fuelpriceint) {
+		totalmpg = milesint/gallonsint;
+		mpgString = String.format("%.2f", totalmpg);
+		
+		mpgtext.setVisibility(View.VISIBLE);
+		mpg.setText(mpgString);
+	}
+	
 }
+
+
+
+
+
+
+
